10cabde74d0bd946a56867e1c959f698dd35aedd
Christopher Marchfelder <marchfelder@googlemail.com>
2012-11-06 07:42:57 +0100

    Replaced the pre-defined forest shapes with a shaper class

--- forest.py
+++ forest.py
@@ -1,35 +1,33 @@
-shape = [
-	[
-		[1, 1, 1],
-		[1, 1, 1]
-	],
-	[
-		[1, 1, 0],
-		[1, 1, 1]
-	],
-	[
-		[0, 1, 1],
-		[1, 1, 1]
-	],
-	[
-		[0, 1, 0],
-		[1, 1, 1],
-		[0, 1, 0]
-	],
-	[
-		[0, 1, 0],
-		[1, 1, 1]
-	],
-	[
-		[1, 1, 1],
-		[0, 1, 0]
-	],
-	[
-		[1, 0, 0],
-		[1, 1, 0]
-	],
-	[
-		[0, 0, 1],
-		[0, 1, 1]
-	]
-]
+from random import choice
+
+class ForestShape():
+    def __init__(self):
+        pass
+    
+    def getShape(self):
+        """Returns the shape of a forest
+        
+        Creates a array with the shape of a forest. This shape will be created randomly each time
+        """
+        # The Dimensions of a forest are either 3 * 3 tiles or 2 * 3
+        width = 3
+        
+        # A very small range (only 2 numbers) makes only one number appear all the time
+        if choice(range(0, 100)) >= 50:
+            height = 3
+        else:
+            height = 2
+        
+        shape = []
+        for i in range(0, height):
+            row = []
+            for j in range(0, width):
+                row.append(choice(range(0, 2)))
+            
+            shape.append(row)
+            
+        return shape
+
+if __name__ == '__main__':
+    shape = ForestShape()
+    print shape.getShape()
--- map.py
+++ map.py
@@ -1,138 +1,142 @@
 from __future__ import print_function
 from random import choice
-from forest import shape
+from forest import ForestShape
+
+# from forest import shape
 
 class Map():
-	
-	# Ready to draw a map element
-	STATE_READY = 1
-	
-	# Started drawing map elements (like a group of trees for a forest9
-	STATE_STARTED = 2
-	
-	# Flag for colored terminal output + the codes
-	USE_CMD_COLOR = False
-	CMD_FORMAT = '\033[%dm%s\033[0m'
-	
-	# Token for Bounds
-	TOKEN_WALL = '#'
-	TOKEN_SPACE = '-'
-	TOKEN_TREE  = 'T'
-	TOKEN_WATER = 'W'
-	TOKEN_MOUNTAIN = 'M'
-	
-	# Dimension of the map
-	size = {'width': 20, 'height': 20}
-	
-	# Coords of the forest
-	forest = None
-	
-	# The map
-	tiles = []
-	
-	def __init__(self, width, height):
-		# Map dimensions will be used to calculate amount of forests, seas and blocks
-		self.size['width'], self.size['height'] = width, height
-		self._calculateForest()
-		self._initColor();
-		
-	def _initColor(self):
-		if self.USE_CMD_COLOR is False:
-			return
-		
-		self.TOKEN_WALL = self.CMD_FORMAT % (30, self.TOKEN_WALL) # Grey
-		self.TOKEN_TREE = self.CMD_FORMAT % (42, self.TOKEN_TREE) # On Green
-	
-	def _calculateForest(self):
-		# todo calc all coords through?
-		
-		# Estimate the amount of forests. The trees should take one quarter of the map, 
-		# each has 6 tiles at max
-		forest = (self.size['width'] + self.size['height'] / 4) / 6;
-		
-		# Find some places where to start with a forest (via random)
-		# Always check that the tree's are within the bounds
-		numForest = len(shape) # used to pick a random forest shape
-		
-		toPlace = []
-		while True:
-			# Find some random coords, -1 for the borders
-			# todo find better coord's, sometimes forest is just to close to each other 
-			x = choice(range(1, self.size['width']-1))
-			y = choice(range(1, self.size['height']-1))
-			
-			# todo check if another forest is within reach/crosses our bounds
-			shapeType = choice(range(0, numForest))
-			
-			# Check bounds
-			if x + 3 >= self.size['width'] or y + len(shape[shapeType]) >= self.size['height']:
-				# Inside border
-				continue;
-			
-			# store
-			toPlace.append({'x': x, 'y': y, 'type': shapeType})
-			
-			# We need one less forest :)
-			forest -= 1
-			
-			if forest == 0:
-				break;
-			
-		self.forest = toPlace
-		
-	def _isBound(self, x, y):
-		"""Returns if the coordinates in the tiles are bounds"""
-		return x == 0 or x == self.size['width'] - 1 or y == 0 or y == self.size['height'] - 1
-	
-	def _hasTree(self, x, y):
-		for i in self.forest:
-			# We always draw from left to right, so check if the forest is within the coord's
-			if i['x'] + 2 >= x and x >= i['x'] and i['y'] + 2 >= y and y >= i['y']:
-				# return True
-				s = shape[i['type']]
-				
-				# The deltas can be used as the array index of the shape
-				deltaY = y - i['y']
-				deltaX = x - i['x']
-				
-				try:
-					# Try to access root key
-					if s[deltaY][deltaX] == 1:
-						return True
-					else:
-						return False
-				except (Exception) as e:
-					return False
+    
+    # Ready to draw a map element
+    STATE_READY = 1
+    
+    # Started drawing map elements (like a group of trees for a forest9
+    STATE_STARTED = 2
+    
+    # Flag for colored terminal output + the codes
+    USE_CMD_COLOR = False
+    CMD_FORMAT = '\033[%dm%s\033[0m'
+    
+    # Token for Bounds
+    TOKEN_WALL = '#'
+    TOKEN_SPACE = '-'
+    TOKEN_TREE  = 'T'
+    TOKEN_WATER = 'W'
+    TOKEN_MOUNTAIN = 'M'
+    
+    # Dimension of the map
+    size = {'width': 20, 'height': 20}
+    
+    # Coords of the forest
+    forest = None
+    
+    # The map
+    tiles = []
+    
+    def __init__(self, width, height):
+        # Map dimensions will be used to calculate amount of forests, seas and blocks
+        self.size['width'], self.size['height'] = width, height
+        self._calculateForest()
+        self._initColor();
+    
+    def _initColor(self):
+        if self.USE_CMD_COLOR is False:
+            return
+        
+        self.TOKEN_WALL = self.CMD_FORMAT % (30, self.TOKEN_WALL) # Grey
+        self.TOKEN_TREE = self.CMD_FORMAT % (42, self.TOKEN_TREE) # On Green
+    
+    def _calculateForest(self):
+        # todo calc all coords through?
+        
+        # Estimate the amount of forests. The trees should take one quarter of the map, 
+        # each has 6 tiles at max
+        forest = (self.size['width'] + self.size['height'] / 4) / 6;
+        
+        # Find some places where to start with a forest (via random)
+        # Always check that the tree's are within the bounds
+        # numForest = len(shape) # used to pick a random forest shape
+        
+        toPlace = []
+        shaper = ForestShape()
+        while True:
+            # Find some random coords, -1 for the borders
+            # todo find better coord's, sometimes forest is just to close to each other 
+            x = choice(range(1, self.size['width']-1))
+            y = choice(range(1, self.size['height']-1))
+            
+            # todo check if another forest is within reach/crosses our bounds
+            # shapeType = choice(range(0, numForest))
+            shape = shaper.getShape()
+            
+            # Check bounds
+            if x + 3 >= self.size['width'] or y + len(shape) >= self.size['height']:
+                # Inside border
+                continue;
+            
+            # store
+            toPlace.append({'x': x, 'y': y, 'shape': shape})
+            
+            # We need one less forest :)
+            forest -= 1
+            
+            if forest == 0:
+                break;
+            
+        self.forest = toPlace
+        
+    def _isBound(self, x, y):
+        """Returns if the coordinates in the tiles are bounds"""
+        return x == 0 or x == self.size['width'] - 1 or y == 0 or y == self.size['height'] - 1
+    
+    def _hasTree(self, x, y):
+        # Todo this could take care of any shape
+        for i in self.forest:
+            # We always draw from left to right, so check if the forest is within the coord's
+            if i['x'] + 2 >= x and x >= i['x'] and i['y'] + 2 >= y and y >= i['y']:
+                s = i['shape']
+                
+                # The deltas can be used as the array index of the shape
+                deltaY = y - i['y']
+                deltaX = x - i['x']
+                
+                try:
+                    # Try to access root key
+                    if s[deltaY][deltaX] == 1:
+                        return True
+                    else:
+                        return False
+                except (Exception) as e:
+                    return False
 
-	def _calculateTiles(self):
-		
-		# Draw bounds
-		for y in xrange(0, self.size['height']):
+    def _calculateTiles(self):
+        
+        # Draw bounds
+        for y in xrange(0, self.size['height']):
 
-			# Tile row
-			row = []
+            # Tile row
+            row = []
 
-			for x in xrange(0, self.size['width']):
-				# Top, bottom, outer left and outer right always have walls
-				if self._isBound(x, y):
-					row.append(self.TOKEN_WALL)
-				else:
-					# We don't want a boring map, what about some water, trees and blocks? 
-					if self._hasTree(x, y):
-						row.append(self.TOKEN_TREE)
-					else:
-						row.append(self.TOKEN_SPACE)
+            for x in xrange(0, self.size['width']):
+                # Top, bottom, outer left and outer right always have walls
+                if self._isBound(x, y):
+                    row.append(self.TOKEN_WALL)
+                else:
+                    # We don't want a boring map, what about some water, trees and blocks? 
+                    if self._hasTree(x, y):
+                        row.append(self.TOKEN_TREE)
+                    else:
+                        row.append(self.TOKEN_SPACE)
 
-			self.tiles.append(row)
+            self.tiles.append(row)
 
-	def _printTiles(self):
-		for t in self.tiles:
-			print(''.join(t))
+    def _printTiles(self):
+        for t in self.tiles:
+            print(''.join(t))
 
-	def render(self):
-		self._calculateTiles()
-		self._printTiles()
+    def render(self):
+        self._calculateTiles()
+        self._printTiles()
 
 if __name__ == '__main__':
-	tileMap = Map(40, 20)
-	tileMap.render()
+    tileMap = Map(40, 20)
+    tileMap.render()
